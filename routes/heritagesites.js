793621438f4c68533ee5b9b9febd439ca7827376
Kousik Kuchipudi <kuchipudi.kousik01@gmail.com>
2024-11-05 14:57:03 -0600

    Add HeritageSites route and views

---
+++ routes/heritagesites.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET heritage sites page. */
+router.get('/', function(req, res, next) {
+  res.render('heritagesites', { title: 'Search Results - HeritageSites' });
+});
+
+module.exports = router;
