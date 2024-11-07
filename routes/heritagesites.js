983537f4cd67ac40289095c164c9dc18c1fa857b
Kousik Kuchipudi <kuchipudi.kousik01@gmail.com>
2024-11-06 19:16:01 -0600

    Added grid

--- routes/heritagesites.js
+++ routes/heritagesites.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET heritage sites page. */
 router.get('/', function(req, res, next) {
-  res.render('heritagesites', { title: 'Search Results - HeritageSites' });
+  res.render('heritagesites', { title: `Search Results - HeritageSites` });
 });
 
 module.exports = router;
